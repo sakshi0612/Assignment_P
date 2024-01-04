253b0ba4adab06ecfbd201025613553ab08b507b
sakshi0612 <155642572+sakshi0612@users.noreply.github.com>
2024-01-04 19:04:28 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,10 @@
+def Num(a):  
+    if a > 0:   
+        print("Number given by you is Positive")  
+    elif a < 0:   
+        print("Number given by you is Negative") 
+    else:   
+        print("Number given by you is zero")   
+a = float(input("Enter a number as input value: "))  
+
+Num(a)  
